384a2145cf8ca6838f2036f8bcf7290033ac6387
Calvin Sprouse <calvin@sprousephotography.com>
2020-02-10 08:33:59 -0800

    Key breaker for APCSP

--- Modulo/ModuloTest.py
+++ Modulo/ModuloTest.py
@@ -1,1 +1,18 @@
-print (13 % 17)
+publicModulus = 2333
+publicKey = 649
+publicNumber = 166
+
+aliceKey = 0
+for i in range (1, publicModulus):
+    if ((publicKey * i) % publicModulus == 1):
+        aliceKey = i
+        break
+
+bobKey = 0
+for i in range (1, publicModulus):
+    if ((publicKey * i) % publicModulus == publicNumber):
+        bobKey = i
+        break
+
+print ("Alice " + str(aliceKey))
+print ("Bob " + str(bobKey))
